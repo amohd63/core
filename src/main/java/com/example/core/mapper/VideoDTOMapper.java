3fd1db72d2a5c8a7c9c245fa4ee625057fa81f90
amohd63 <amohdmari63@gmail.com>
2024-02-19 21:35:23 +0200

    - Mapper not working

--- src/main/java/com/example/core/mapper/VideoDTOMapper.java
+++ src/main/java/com/example/core/mapper/VideoDTOMapper.java
@@ -8,17 +8,21 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
 @Component
-public class VideoDTOMapper {
+@Mapper(componentModel = "spring")
+public interface VideoDTOMapper {
+    VideoDTOMapper INSTANCE = Mappers.getMapper(VideoDTOMapper.class);
 
-    public VideoDTO videoToVideoDTO(Video video){
-        return new VideoDTO(
-                video.getVideo_id(), video.getTitle(), video.getUrl(), video.getLength(), video.getVideo_setting_id()
-        );
-    }
+    @Mapping(target = "vId", source = "entity.video_id")
+    @Mapping(target = "title", source = "entity.title")
+    @Mapping(target = "URL", source = "entity.url")
+    @Mapping(target = "length", source = "entity.length")
+    @Mapping(target = "sId", source = "entity.video_setting_id")
+    VideoDTO videoToVideoDTO(Video video);
 
-    public Video videoDTOToVideo(VideoDTO videoDTO){
-        return new Video(
-                videoDTO.getVId(), videoDTO.getTitle(), videoDTO.getURL(), videoDTO.getLength(), videoDTO.getSId()
-        );
-    }
+    @Mapping(source = "dto.vId", target = "video_id")
+    @Mapping(source = "dto.title", target = "title")
+    @Mapping(source = "dto.URL", target = "url")
+    @Mapping(source = "dto.length", target = "length")
+    @Mapping(source = "dto.sId", target = "video_setting_id")
+    Video videoDTOToVideo(VideoDTO videoDTO);
 }
