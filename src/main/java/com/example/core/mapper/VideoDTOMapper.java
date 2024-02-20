7d42ae03d3d8ba531d5d45fb4d60d71b7cdceaca
Ali Mohammed <81199901+amohd63@users.noreply.github.com>
2024-02-20 15:03:03 +0200

    -

--- src/main/java/com/example/core/mapper/VideoDTOMapper.java
+++ src/main/java/com/example/core/mapper/VideoDTOMapper.java
@@ -4,21 +4,20 @@ import com.example.core.controller.DTO.VideoDTO;
 import com.example.core.model.Video;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Component;
 
-@Component
-public class VideoDTOMapper {
+@Mapper(componentModel = "spring")
+public interface VideoDTOMapper {
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "title", source = "title")
+    @Mapping(target = "url", source = "url")
+    @Mapping(target = "length", source = "length")
+    @Mapping(target = "video_settings", source = "video_settings")
+    VideoDTO videoToVideoDTO(Video video);
 
-    public VideoDTO videoToVideoDTO(Video video){
-        return new VideoDTO(
-                video.getVideo_id(), video.getTitle(), video.getUrl(), video.getLength()
-        );
-    }
-
-    public Video videoDTOToVideo(VideoDTO videoDTO){
-        return new Video(
-                videoDTO.getVId(), videoDTO.getTitle(), videoDTO.getURL(), videoDTO.getLength(), null
-        );
-    }
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "url", target = "url")
+    @Mapping(source = "length", target = "length")
+    @Mapping(source = "video_settings", target = "video_settings")
+    Video videoDTOToVideo(VideoDTO videoDTO);
 }
